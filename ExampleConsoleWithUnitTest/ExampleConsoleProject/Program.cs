4e9a0a015c58068d851c316ebadd63dab1eb8515
VladlenaMarkvirer <markvirervd@gmail.com>
2025-02-16 15:54:50 +0500

    Add some logic in Program and UI methods

--- ExampleConsoleWithUnitTest/ExampleConsoleProject/Program.cs
+++ ExampleConsoleWithUnitTest/ExampleConsoleProject/Program.cs
@@ -1,14 +1,81 @@
 ﻿using LogicProject;
+using ControllerProject;
 
 namespace ExampleConsoleProject
 {
 
     class Program
     {
+        public static void ShowMoney(Money money)
+        {
+            Console.WriteLine(money.MoneyInfo());
+        }
+        public static void ShowMessage(string message, bool error = false)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            if (error)
+                Console.ForegroundColor = ConsoleColor.Red;
+            else
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+        public static void IncreaseRubles(ref Money money)
+        {
+            string message = Controller.TryIncreaseRubles(ref money);
+            if (message == "")
+            {
+                ShowMoney(money);
+            }
+            else
+            {
+                ShowMessage(message, true);
+            }
+        }
+        public static void SumMas(int[] mas)
+        {
+            int result;
+            string error = Controller.TrySumMas(mas, out result);
+            if (error == "")
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                ShowMessage(error, true);
+            }
+        }
+
+        public static double SumMoneysInRubles(Money[] moneys)
+        {
+            double result = 0.0;
+            foreach (Money money in moneys)
+            {
+                result += money.Rub + money.Kop / 100.0;
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            int summa = Mathematics.SumElements(new int[] { });
-            Console.WriteLine(summa);
+            ShowMessage("----| Проверка суммирования элементов массива |----");
+            SumMas(new int[] { 1, 2, 3, 4, 5 });
+            SumMas(new int[] { 2000000000, 2000000000 });
+            SumMas(new int[] { });
+            ShowMessage("------| Создание трёх элементов типа Money |------");
+            Money m1 = new Money(10, 5);
+            Money m2 = new Money(100, 5);
+            Money m3 = new Money(10, 105);
+            ShowMoney(m1);
+            ShowMoney(m2);
+            ShowMoney(m3);
+            ShowMessage("---| Добавление рубля к каждому объекту типа Money |---");
+            IncreaseRubles(ref m1);
+            IncreaseRubles(ref m2);
+            IncreaseRubles(ref m3);
+            ShowMessage("---| Сумма в рублях всех Money в массиве |---");
+            Money[] moneys = new Money[] { m1, m2, m3 };
+            Console.WriteLine(SumMoneysInRubles(moneys));
         }
     }
 
--- ExampleConsoleWithUnitTest/LogicProject/obj/Debug/net8.0/LogicProject.AssemblyInfo.cs
+++ ExampleConsoleWithUnitTest/LogicProject/obj/Debug/net8.0/LogicProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76a956f714f6f95f5dd14aaae988c639e2bc4dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acad1bc369442d65ab8cab2787c0934197748f7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
